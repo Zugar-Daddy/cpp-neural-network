427b3f8d389f9bf39fd65866194815a21e937479
Madhav <69211934+Zugar-Daddy@users.noreply.github.com>
2026-09-13 20:02:40 +0530

    I see numbers and labels :)

--- Loader.cpp
+++ Loader.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-void shift_bits(char* bigEndian, int size){
+void change_endian(char* bigEndian, int size){
     char* end = bigEndian + size - 1;
     for(int i = 0; i < size / 2; i++){
         swap(*(bigEndian + i), *(end - i));
@@ -30,34 +30,46 @@ int main(){
     char num_images[4]; image_file.read(num_images, 4);
     char rows[4]; image_file.read(rows, 4);
     char cols[4]; image_file.read(cols, 4);
+    char* images = new char[60000*784];
+    image_file.read(images, 784 * 60000);
 
-    shift_bits(magic_number, 4);
-    shift_bits(num_images, 4);
-    shift_bits(rows, 4);
-    shift_bits(cols, 4);
+    change_endian(magic_number, 4);
+    change_endian(num_images, 4);
+    change_endian(rows, 4);
+    change_endian(cols, 4);
 
-    // Labels
+    // Labels header
     char magic[4]; labels_file.read(magic, 4);
     char num[4]; labels_file.read(num, 4);    
-    shift_bits(magic, 4);
-    shift_bits(num, 4);
+    char* labels = new char[60000]; 
+    change_endian(magic, 4);
+    change_endian(num, 4);
 
-    
-    // uint8_t* buffer = new uint8_t[784];
+    labels_file.read(labels, 60000);    
 
+
+    // DEBUG
     cout << "Magic: "  << *reinterpret_cast<uint32_t*>(magic_number) << "\n";
     cout << "Images: " << *reinterpret_cast<uint32_t*>(num_images) << "\n";
     cout << "Rows: "   << *reinterpret_cast<uint32_t*>(rows) << "\n";
     cout << "Cols: "   << *reinterpret_cast<uint32_t*>(cols) << "\n";
-
-    cout << '\n';
-    
+    cout << '\n';    
     cout << "Magic: "  << *reinterpret_cast<uint32_t*>(magic) << "\n";
     cout << "Images: " << *reinterpret_cast<uint32_t*>(num) << "\n";
-
-
-
-
+    
+    int c = 0;
+    for(int j = 0; j < 5; j++){
+        cout << '\n' << (int)(unsigned char)labels[j] << '\n' << '\n';
+        for(int i = 0; i < 784; i++){
+            if((int)(unsigned char)images[(784 * j) + i] > 128) cout << '#';
+            else cout << ".";
+            c++;
+            if(c == 28){
+                c = 0;
+                cout << '\n';
+            }
+        }
+    }
 
 
     image_file.close(); labels_file.close();
